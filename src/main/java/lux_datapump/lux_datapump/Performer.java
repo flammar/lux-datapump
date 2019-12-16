5c386af74dd980ba93110b9910e4a6e5459a86e8
Michael Skidan <michael.skidan@googlemail.com>
2019-12-17 00:02:51 +0300

    1

--- src/main/java/lux_datapump/lux_datapump/Performer.java
+++ src/main/java/lux_datapump/lux_datapump/Performer.java
@@ -1,8 +1,30 @@
 package lux_datapump.lux_datapump;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Performer {
 	
-	public void perform() {
+	public int[] perform(Connection connection/* = getDatabaseConnection();*/, String query, Object[][] data) throws SQLException {
+		
+//        connection.setAutoCommit(true);
+		
+        ;
+
+		
+		try(PreparedStatement statement = connection.prepareStatement(query)) {
+			for (int index = 0; index < data.length; index++) {
+				Object[] objects = data[index];
+				for (int j = 0; j < objects.length; j++) {
+					statement.setObject(j + 1, objects[j]);
+				}
+				statement.addBatch();
+			}
+			return statement.executeBatch();
+		} finally {
+			
+		} 
 		
 	}
 
