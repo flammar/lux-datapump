43e61052614b5a7ec3dcd526c184ddc5a2b33119
Michael Skidan <michael.skidan@googlemail.com>
2019-12-23 19:58:23 +0300

    1

--- src/main/java/lux_datapump/lux_datapump/Performer.java
+++ src/main/java/lux_datapump/lux_datapump/Performer.java
@@ -3,10 +3,15 @@ package lux_datapump.lux_datapump;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.AbstractList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Performer {
 	
-	public int[] perform(Connection connection/* = getDatabaseConnection();*/, String query, Object[][] data) throws SQLException {
+	
+	public List<Integer> perform(Connection connection/* = getDatabaseConnection();*/, String query, List<List<Object>> data) throws SQLException {
 		
 //        connection.setAutoCommit(true);
 		
@@ -14,14 +19,27 @@ public class Performer {
 
 		
 		try(PreparedStatement statement = connection.prepareStatement(query)) {
-			for (int index = 0; index < data.length; index++) {
-				Object[] objects = data[index];
-				for (int j = 0; j < objects.length; j++) {
-					statement.setObject(j + 1, objects[j]);
+			for (List<Object> oo: data) {
+				int i = 1;
+				// TODO
+				for (Object o : oo) {
+					statement.setObject(i++, o);
 				}
 				statement.addBatch();
-			}
-			return statement.executeBatch();
+			};
+			int[] executeBatch = statement.executeBatch();
+			return new AbstractList<Integer>() {
+
+				@Override
+				public Integer get(int index) {
+					return Integer.valueOf(executeBatch[index]);
+				}
+
+				@Override
+				public int size() {
+					return executeBatch.length;
+				}
+			};
 		} finally {
 			
 		} 
