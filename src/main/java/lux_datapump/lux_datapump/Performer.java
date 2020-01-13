a5c053761f7ea1cd137c610cac1a88fc89f0b3ef
Michael Skidan <michael.skidan@googlemail.com>
2020-01-13 18:36:54 +0300

    throwing functions, consumers wrapped, Abstract Lists wrapped

--- src/main/java/lux_datapump/lux_datapump/Performer.java
+++ src/main/java/lux_datapump/lux_datapump/Performer.java
@@ -1,5 +1,9 @@
 package lux_datapump.lux_datapump;
 
+import static lux_datapump.lux_datapump.Utils.list;
+import static lux_datapump.lux_datapump.Utils.unbox;
+import static lux_datapump.lux_datapump.Utils.wrap;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,10 +21,13 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import lux_datapump.lux_datapump.Limit.Type;
+import lux_datapump.lux_datapump.Utils.ThrowingFunction;
 
 public class Performer {
 	private Integer portionSize;
@@ -47,7 +54,8 @@ public class Performer {
 			dstColDescs = getColDescs(dstConn, dstTableName);
 			final String dstColList = String.join(", ",
 					dstColDescs.stream().map(d -> d.dstName).collect(Collectors.toList()));
-			final List<String> dstValList = dstColDescs.stream().map(d -> d.dstExpr).collect(Collectors.toList());//Collections.nCopies(dstColDescs.size(), "?");
+			final List<String> dstValList = dstColDescs.stream().map(d -> d.dstExpr).collect(Collectors.toList());// Collections.nCopies(dstColDescs.size(),
+																													// "?");
 			final String dstQuery = MessageFormat.format("INSERT INTO {0} ( {1} ) VALUES ( {2} )", dstTableName,
 					dstColList, String.join(", ", dstValList));
 			final String srcColList = String.join(", ",
@@ -56,16 +64,11 @@ public class Performer {
 					Statement srcStatement = srcConn.createStatement();
 					ResultSet src = srcStatement.executeQuery(
 							MessageFormat.format("SELECT {1} FROM {0}{2}", srcTableName, srcColList, where));) {
-				Iterator<List<ValueDescriptor>> baseIterator = Utils.toIterator(src, dstColDescs/*.size()*/);
-				Utils.portionize(baseIterator, this.portionSize).forEachRemaining( data -> {
-					try {
-						perform(dstConn, dstQuery, data);
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				});
-				
-				
+				Iterator<List<ValueDescriptor>> baseIterator = Utils.toIterator(src, dstColDescs/* .size() */);
+				Utils.portionize(baseIterator, this.portionSize).forEachRemaining(wrap(data -> {
+					perform(dstConn, dstQuery, data);
+				}));
+
 			} finally {
 
 			}
@@ -85,26 +88,27 @@ public class Performer {
 				ResultSet executeQuery = preStt
 						.executeQuery(MessageFormat.format("SELECT * FROM {0} WHERE 0 = 1", tableName));) {
 			final ResultSetMetaData metaData = executeQuery.getMetaData();
-			final ArrayList<ColumnDescriptor> arrayList = new ArrayList<ColumnDescriptor>(
-					new AbstractList<ColumnDescriptor>() {
-						int columnCount = metaData.getColumnCount();
-
-						@Override
-						public ColumnDescriptor get(final int index) {
-							try {
-								final int i = index + 1;
-								return new ColumnDescriptor(i, metaData.getCatalogName(i),
-										metaData.getColumnType(i));
-							} catch (final SQLException e) {
-							}
-							return null;
-						}
-
-						@Override
-						public int size() {
-							return columnCount;
-						}
-					});
+			// new AbstractList<ColumnDescriptor>() {
+//
+//				@Override
+//				public ColumnDescriptor get(final int index) {
+//					try {
+//						final int i = index + 1;
+//						return new ColumnDescriptor(i, metaData.getCatalogName(i), metaData.getColumnType(i));
+//					} catch (final SQLException e) {
+//					}
+//					return null;
+//				}
+//
+//				@Override
+//				public int size() {
+//					return columnCount;
+//				}
+//			};
+			final ArrayList<ColumnDescriptor> arrayList = new ArrayList<ColumnDescriptor>(list(
+					unbox(wrap((ThrowingFunction<Integer, ColumnDescriptor>) ((i) -> new ColumnDescriptor(i + 1,
+							metaData.getCatalogName(i + 1), metaData.getColumnType(i + 1))))),
+					metaData.getColumnCount()));
 			return arrayList;
 		} finally {
 		}
@@ -113,40 +117,24 @@ public class Performer {
 	public List<Integer> perform(final Connection connection/* = getDatabaseConnection(); */, final String query,
 			final Iterable<List<ValueDescriptor>> data) throws SQLException {
 		try (PreparedStatement statement = connection.prepareStatement(query)) {
-//			for (final List<ValueDescriptor> oo : data) {
-//				int i = 1;
-//				for (final Object o : oo) {
-//					setObject(statement, i++, o);
-//				}
-//				statement.addBatch();
-//			}
-//			;
-			data.forEach(vds -> {vds.forEach(vd->{
-				try {
-					vd.set(statement);
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			});try {
+			data.forEach(wrap(vds -> {
+				vds.forEach(wrap((ValueDescriptor vd) -> vd.set(statement)));
 				statement.addBatch();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}});
+			}));
 			final int[] executeBatch = statement.executeBatch();
-			return new AbstractList<Integer>() {
-
-				@Override
-				public Integer get(final int index) {
-					return Integer.valueOf(executeBatch[index]);
-				}
-
-				@Override
-				public int size() {
-					return executeBatch.length;
-				}
-			};
+			// new AbstractList<Integer>() {
+//
+//				@Override
+//				public Integer get(final int index) {
+//					return Integer.valueOf(executeBatch[index]);
+//				}
+//
+//				@Override
+//				public int size() {
+//					return executeBatch.length;
+//				}
+//			};
+			return list(index -> Integer.valueOf(executeBatch[index]), executeBatch.length);
 		} finally {
 
 		}
--- src/main/java/lux_datapump/lux_datapump/Utils.java
+++ src/main/java/lux_datapump/lux_datapump/Utils.java
@@ -6,55 +6,92 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
 
 public class Utils {
 
+	public static interface ThrowingConsumer<T> extends Consumer<T> {
+		@Override
+		default void accept(final T elem) {
+			try {
+				throwingAccept(elem);
+			} catch (final Exception e) {
+				// Implement your own exception handling logic here..
+				// For example:
+//		            System.out.println("handling an exception...");
+				// Or ...
+				throw new RuntimeException(e);
+			}
+		}
+
+		void throwingAccept(T elem) throws Exception;
+	}
+
+	public static interface ThrowingFunction<T, R> extends Function<T, R> {
+		@Override
+		default R apply(final T elem) {
+			try {
+				return applyThrows(elem);
+			} catch (final Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		R applyThrows(T elem) throws Exception;
+	}
+
 	private Utils() {
 	}
 
-	static Iterator<List<ValueDescriptor>> toIterator(ResultSet src, List<ColumnDescriptor> descs/*int size*/) {
-			return new Iterator<List<ValueDescriptor>>() {
-	//			private final int size = ;
-				private boolean wasNext = false, wentOut = false;
-				private List<ValueDescriptor> current = null; 
-	
-				@Override
-				public boolean hasNext() {
-					return tryNext(src);
-				}
-	
-				@Override
-				public List<ValueDescriptor> next() {
-					if(! hasNext()) return null;
-					return current;
-				}
-	
-				private boolean tryNext(ResultSet src) {
-					if(!wentOut && !wasNext) {
-						try {
-							wentOut = !src.next();
-							current = wentOut ? null : new ArrayList<>(rowToList());
-							wasNext = true;
-						} catch (SQLException e) {
-							wentOut=true;
-							current = null;
-						}
+	public static Iterator<List<ValueDescriptor>> toIterator(ResultSet src, List<ColumnDescriptor> descs/* int size */) {
+		return new Iterator<List<ValueDescriptor>>() {
+			// private final int size = ;
+			private boolean wasNext = false, wentOut = false;
+			private List<ValueDescriptor> current = null;
+
+			@Override
+			public boolean hasNext() {
+				return tryNext(src);
+			}
+
+			@Override
+			public List<ValueDescriptor> next() {
+				if (!hasNext())
+					return null;
+				return current;
+			}
+
+			private boolean tryNext(ResultSet src) {
+				if (!wentOut && !wasNext) {
+					try {
+						wentOut = !src.next();
+						current = wentOut ? null : new ArrayList<>(rowToList());
+						wasNext = true;
+					} catch (SQLException e) {
+						wentOut = true;
+						current = null;
 					}
-					return !wentOut;
 				}
-	
-				private List<ValueDescriptor> rowToList() {
+				return !wentOut;
+			}
+
+			private List<ValueDescriptor> rowToList() {
 //					final ResultSet src1 = src;
-					return descs.stream().map(c-> {
-						try {
-							return ValueDescriptor.create(src, c);
-						} catch (SQLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-							return null;
-						}
-					}).collect(Collectors.toList());
+				return descs.stream().map(c -> {
+					try {
+						return ValueDescriptor.create(src, c);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						return null;
+					}
+				}).collect(Collectors.toList());
 //					return new AbstractList<Object>() {
 //					
 //						
@@ -73,37 +110,37 @@ public class Utils {
 //							return descs.size();
 //						}
 //					};
-				}
-			};
-		}
+			}
+		};
+	}
 
-	static <T> Iterator<Iterable<T>> portionize(Iterator<T> baseIterator, int portionSize) {
+	public static <T> Iterator<Iterable<T>> portionize(Iterator<T> baseIterator, int portionSize) {
 		return new Iterator<Iterable<T>>() {
-	
+
 			@Override
 			public boolean hasNext() {
 				return baseIterator.hasNext();
 			}
-	
+
 			@Override
 			public Iterable<T> next() {
 				return new Iterable<T>() {
-					private int i=portionSize;
+					private int i = portionSize;
 					private final Iterator<T> base = baseIterator;
-					
+
 					@Override
 					public Iterator<T> iterator() {
 						return new Iterator<T>() {
-							
+
 							@Override
 							public T next() {
 								i--;
 								return base.hasNext() ? base.next() : null;
 							}
-							
+
 							@Override
 							public boolean hasNext() {
-								return i >0 && base.hasNext();
+								return i > 0 && base.hasNext();
 							}
 						};
 					}
@@ -111,7 +148,67 @@ public class Utils {
 			}
 		};
 	}
-	
-	
 
+	public static <R> IntFunction<R> unbox(Function<Integer, R> base) {
+		return new IntFunction<R>(){
+
+			@Override
+			public R apply(int value) {
+				return base.apply(value);
+			}};
+	}
+
+	public static <U, V> Function<U, V> wrap(ThrowingFunction<U, V> base) {
+		return base;
+	}
+
+	public static <U, R, X extends Exception> Function<U, R> wrap(ThrowingFunction<U, R> base, BiFunction<U, X, R> xh) {
+		return new ThrowingFunction<U, R>() {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public R applyThrows(U elem) throws Exception {
+				final U elem1 = elem;
+				try {
+					return base.applyThrows(elem1);
+				} catch (final Exception e) {
+					return xh.apply(elem, (X) e);
+				}
+			}
+
+			@Override
+			public R apply(U elem) {
+				return ThrowingFunction.super.apply(elem);
+			}
+		};
+	}
+
+
+	public static <U> Consumer<U> wrap(ThrowingConsumer<U> base) {
+		return base;
+	}
+
+	public static <U, X extends Exception> Consumer<U> wrap(ThrowingConsumer<U> base, BiConsumer<U, X> xh) {
+		return base;
+	}
+
+	public static <C> AbstractList<C> list (IntFunction<C> item, IntSupplier size){
+		return new AbstractList<C>() {
+
+			@Override
+			public C get(int index) {
+				return item.apply(index);
+			}
+
+			@Override
+			public int size() {
+				return size.getAsInt();
+			}
+		};
+		
+	}
+	
+	public static <C> AbstractList<C> list (IntFunction<C> item, int size){
+		return list(item, ()->size);
+	}
 }
